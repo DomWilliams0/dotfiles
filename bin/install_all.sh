d7b98e198562d826da6e5d2e8fce1e83d5eab3cd
Dom Williams <me@domwillia.ms>
2016-03-01 11:40:05 +0000

    Fixes for auto installation

--- bin/install_all.sh
+++ bin/install_all.sh
@@ -7,3 +7,6 @@ sudo pacman -S zsh
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 sudo sh -c "$(wget https://raw.githubusercontent.com/DomWilliams0/wallpaper-fetcher/master/install.sh -O -)"
+
+sudo pacman-key --recv-keys AE6866C7962DDE58
+sudo pacman-key --refresh-keys
